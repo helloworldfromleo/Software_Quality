02f74896ba2a9ae3d50f1b71baea3af2d27bf6e3
Leonhard Blank <leonhard.blank@tech-excellence.de>
2024-10-31 11:11:49 +0100

    My Math

---
+++ MyMaths.java
@@ -0,0 +1,25 @@
+package qm;
+
+public class MyMaths {
+	
+	public int gcd(int m, int n) {
+		
+		int r;
+		if (n>m) {
+			
+			r = m;
+			m = n;
+			n = r;
+		}
+		
+		r = m % n;
+		while (r != 0) {
+			
+			m = n;
+			n = r;
+			r = m % n;
+		}		
+		return n;
+	}
+	
+}
